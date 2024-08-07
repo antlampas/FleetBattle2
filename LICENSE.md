e77a2e6bc763d76b6553e0cd9c33f8b9bb90989c
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-07 15:40:31 +0200

    -

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+FALP Fleet Battle 2 © 2024 by Antlampas is licensed under CC BY-SA 4.0
--- include/types.hpp
+++ include/types.hpp
@@ -1,6 +1,6 @@
 /*
  *
- * FALP Fleet Battle © 2024 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ * FALP Fleet Battle 2 © 2024 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
  *
  */
 

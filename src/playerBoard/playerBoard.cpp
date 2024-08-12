e36f50cf65c8f40bad49ca32c191800446e86675
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-12 22:06:41 +0200

    -

---
+++ src/playerBoard/playerBoard.cpp
@@ -0,0 +1,11 @@
+/*
+ *
+ * FALP Fleet Battle 2 © 2024 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+ namespace fleetBattle
+ {
+    playerBoard(BoardType shipsOverlay) : shipsOverlay(shipsOverlay){}
+    ~playerBoard(){}
+ }

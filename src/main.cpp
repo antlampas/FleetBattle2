b89ec0a7ebfdbce3f1eb4f7cd85731cfc464f92e
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-14 14:48:01 +0200

    -

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*
+ *
+ * FALP Fleet Battle 2 © 2024 by Antlampas is licensed under CC BY-SA 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+ *
+ */
+
+int main(int argc,char** argv)
+{
+    return 0;
+}

c4ec214dabb853872ef4395ac69ba13012e64357
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-18 12:17:24 +0200

    -

--- src/playerBoard/playerBoard.cpp
+++ src/playerBoard/playerBoard.cpp
@@ -8,7 +8,22 @@
 
 namespace fleetBattle
 {
-    playerBoard::playerBoard(BoardType shipsOverlay)  : shipsOverlay{shipsOverlay}{};
+    playerBoard::playerBoard(std::vector<ShipPosition> ships)
+    {
+        for(int row: {0,1,2,3,4,5,6,7,8,9})
+        {
+            this->_board.push_back(ColumnType());
+            for(int column: {0,1,2,3,4,5,6,7,8,9})
+                this->_board.at(row).push_back(SquareStatus.WATER);
+        }
+
+        for(auto ship: ships)
+        {
+            for(int row=ship.first.first;row<=ship.second.first;row++)
+                for(int column=ship.first.second;column<=ship.second.second;column++)
+                    this->_board.at(row).at(column) = SquareStatus.SHIP;
+        }
+    };
     playerBoard::playerBoard()
     {
         for(int row: {0,1,2,3,4,5,6,7,8,9})

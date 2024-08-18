9fb194750b5328568b1cb53f7380045d99cd0d37
antlampas <francesco.antonettilamorgesepasseri@protonmail.com>
2024-08-18 19:40:31 +0200

    -

--- src/player/checkShoot.cpp
+++ src/player/checkShoot.cpp
@@ -12,6 +12,66 @@ namespace fleetBattle
     {
         this->ownBoard.setSquareStatus(coordinates,this->ownBoard.getSquareStatus(coordinates)+32);
 
+        int coordinatesRow    = coordinates.first;
+        int coordinatesColumn = coordinates.second;
+
+        for(auto ship; this->deployedShips)
+        {
+            int shipInitialRow    = ship.first.first;
+            int shipEndRow        = ship.second.first;
+            int shipInitialColumn = ship.first.second;
+            int shipEndColumn     = ship.second.second;
+            
+            if(isHorizontal(ship))
+            {
+                int shipRow           = ship.first.first;
+                int shipInitialColumn = ship.first.second;
+                int shipEndColumn     = ship.second.second;
+
+                if(coordinatesRow == shipInitialRow)
+                {
+                    if(shipInitialColumn < shipEndColumn)
+                    {
+                        if(coordinatesColumn > shipInitialColumn && coordinatesColumn < shipEndColumn)
+                        {
+                            this->isShipSunk(ship);
+                        }
+                    }
+                    else
+                    {
+                        if(coordinatesColumn > shipEndColumn && coordinatesColumn < shipInitialColumn)
+                        {
+                            this->isShipSunk(ship);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                int shipInitialRow = ship.first.first;
+                int shipEndRow     = ship.second.first;
+                int shipColumn     = ship.first.second;
+
+                if(coordinatesColumn == shipInitialColumn)
+                {
+                    if(shipInitialRow < shipEndRow)
+                    {
+                        if(coordinatesRow > shipInitialRow && coordinatesRow < shipEndRow)
+                        {
+                            this->isShipSunk(ship);
+                        }
+                    }
+                    else
+                    {
+                        if(coordinatesRow > shipEndRow && coordinatesRow < shipInitialRow)
+                        {
+                            this->isShipSunk(ship);
+                        }
+                    }
+                }
+            }
+        }
+
         return this->ownBoard.getSquareStatus(coordinates);
     }
 }
